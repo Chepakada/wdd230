fcef10bbbd46390641eb3bf3e6591f31b35bec23
Prashant Lamichhane <prashantlamichhane68@gmail.com>
2023-05-09 14:35:04 -0600

    tablebuild

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -33,3 +33,11 @@ document.getElementById("last-modified").innerHTML = `Last Modified : ${nlastMod
 // }
 
 // getWeater(url)
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+    navigation.setAttribute("display", "show")
+});
