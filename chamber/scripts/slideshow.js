b179a5b11ad351d876bd64d183f8c14f46ff2a3b
Prashant Lamichhane <prashantlamichhane68@gmail.com>
2023-05-14 08:49:52 -0600

    slideshows added

---
+++ chamber/scripts/slideshow.js
@@ -0,0 +1,18 @@
+const slides = ["hero.jpeg", "slide2.jpeg", "slide3.jpeg", "slide4.jpeg", "slide5.jpeg"];
+
+var xForSlide = () => {
+    const time = new Date().getSeconds();
+    return time;
+}
+
+function slideshow(slides){
+    const interval = xForSlide();
+    console.log(slides[interval % slides.length])
+    document.querySelector(".hero-image").src = `./images/slideshow/${slides[interval % slides.length]}`;
+}
+
+
+
+setInterval(function() {
+    slideshow(slides);
+}, 2000);
