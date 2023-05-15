b3841374c9ed45cfa6f9c3020217832cadf0c094
Prashant Lamichhane <prashantlamichhane68@gmail.com>
2023-05-15 07:35:23 -0600

    added hover and link to cardboxes

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,2 +1,8 @@
 const date = new Date();
 document.getElementById("year").innerText = date.getFullYear();
+
+const cardBoxes = document.querySelectorAll(".card-box")
+
+cardBoxes.forEach(cardBox => cardBox.addEventListener("click", ()=>{
+    window.location.href = "./index.html"
+}))
